997fa43228dc15775af969627f7b8e2edcceaf36
sponticelli <sandro.ponticelli@gmail.com>
2022-07-23 18:43:26 +0200

    feat: Pause System

--- Assets/Scripts/LiteNinja/Systems/Abstracts/ASystem.cs
+++ Assets/Scripts/LiteNinja/Systems/Abstracts/ASystem.cs
@@ -4,11 +4,21 @@ namespace LiteNinja.Systems
 {
     public abstract class ASystem : MonoBehaviour, ISystem
     {
+        private bool _isInitialized;
+
         protected void Awake()
         {
             Initialize();
         }
 
-        public abstract void Initialize();
+        public void Initialize()
+        {
+            if (_isInitialized) return;
+
+            OnLoadSystem();
+            _isInitialized = true;
+        }
+
+        protected abstract void OnLoadSystem();
     }
 }
---
+++ Assets/Scripts/Maestro/Systems/PauseSystem.cs
@@ -0,0 +1,37 @@
+using System;
+using LiteNinja.SOEvents;
+using LiteNinja.Systems;
+using UnityEngine;
+
+namespace Maestro.Systems
+{
+    [AddComponentMenu("Maestro/Systems/Pause System")]
+    public class PauseSystem : ASystem
+    {
+        [Header("Listened Events")]
+        [SerializeField] private GameEvent _requestPause;
+        
+        [Header("Raised Events")]
+        [SerializeField] private BoolEvent _pauseEvent;
+
+        private bool _isPaused;
+        
+        protected override void OnLoadSystem()
+        {
+            _requestPause?.Register(OnRequestPause);
+        }
+
+        private void SetPaused(bool state)
+        {
+            _isPaused = state;
+            _pauseEvent?.Raise(state);
+        }
+        
+        private void OnRequestPause()
+        {
+            _isPaused = !_isPaused;
+            SetPaused(_isPaused);
+        }
+        
+    }
+}
