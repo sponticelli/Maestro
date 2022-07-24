2760ffe10b2596ae53ef2c34580b36c3732fb536
sponticelli <sandro.ponticelli@gmail.com>
2022-07-24 19:05:17 +0200

    feat: small particle dust when player changes direction

---
+++ Assets/Scripts/Maestro/Players/Visuals/PlayerDust.cs
@@ -0,0 +1,38 @@
+using System;
+using LiteNinja.MathUtils;
+using UnityEngine;
+
+namespace Maestro.Players
+{
+    public class PlayerDust : MonoBehaviour, IMove
+    {
+        [SerializeField] private ParticleSystem _particleSystem;
+        
+        
+        private double _currentAngle;
+        private double _previousAngle;
+
+        private void Awake()
+        {
+            if (!_particleSystem)
+                _particleSystem = GetComponent<ParticleSystem>();
+        }
+
+        public void Move(Vector2 direction, float speed)
+        {
+            _currentAngle = MathHelper.Angle(Vector3.zero, direction);
+            var deltaAngle = _currentAngle - _previousAngle;
+            if (deltaAngle > 180) deltaAngle -= 360;
+            if (deltaAngle < -180) deltaAngle += 360;
+            deltaAngle = Math.Abs(deltaAngle);
+            
+            if (deltaAngle > 30)
+            {
+                if (!_particleSystem.isPlaying)
+                    _particleSystem.Play();
+            }
+            
+            _previousAngle = _currentAngle;
+        }
+    }
+}
